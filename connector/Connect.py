bb263f1fc34866d4b8230ce018dd57a5b2812acb
ThomasDelamarre <thomas.delamarre@student.ecp.fr>
2020-02-07 12:11:37 +0100

    Méthodes send
    Ajout config.json
    Ajout méthode send

--- connector/Connect.py
+++ connector/Connect.py
@@ -1,15 +1,21 @@
 import socket
+import sys
+import json
 from time import sleep, time
 from threading import Thread
 
-IP = "127.0.0.1"
-port = 5555
+def get_command(sock):
+    command = bytes()
+    while len(command)<3:
+        command += sock.recv(3-len(command))
+    return command.decode()
 
-def getcommand(sock):
-    commande = bytes()
-    while len(commande)<3:
-        commande += sock.recv(3-len(commande))
-    return commande.decode()
+def receive_bye(sock):
+    sock.close()
+
+def receive_end(sock):
+    #supprimertoutcequiexiste
+    pass
 
 def send_nme(sock, name):
     message = bytes()
@@ -18,16 +24,27 @@ def send_nme(sock, name):
     message += name.encode()
     sock.send(message)
 
+def send_mov(sock, movements):
+    message = bytes()
+    message += 'MOV'.encode()
+    message += bytes([len(movements)])
+    for mov in movements:
+        message += mov.coord_init.encode()
+        message += mov.number_indiv.encode()
+        message += mov.coord_arriv.encode()
+    sock.send(message)
+
 
 def run():
+    file = open("config.json", "r")
+    config = json.load(file)
+    IP, port = config["IP"], int(config["port"])
 
-    #Ajouter ici le fichier de config
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect(("localhost", 555))
+    sock.connect((IP, port))
     print("Connect")
-
     send_nme(sock, "test")
-    print(getcommand(sock))
+    print(get_command(sock))
 
 
