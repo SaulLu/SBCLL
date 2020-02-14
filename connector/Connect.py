2e7d9185cb7af8b23851d5c966fa3230bdbe8f0e
ThomasDelamarre <thomas.delamarre@student.ecp.fr>
2020-02-14 11:52:56 +0100

    Fix send-mov function

--- connector/Connect.py
+++ connector/Connect.py
@@ -1,10 +1,10 @@
 import socket
-import sys
 import json
 import pathlib
 import argparse
-from time import sleep, time
-from threading import Thread
+import time
+
+
 
 def load_config():
     config_path = pathlib.Path(__file__).parent / "config.json"
@@ -12,23 +12,28 @@ def load_config():
         content = json.load(f)
     return content
 
+
 def get_command(sock):
     command = bytes()
-    while len(command)<3:
-        command += sock.recv(3-len(command))
+    while len(command) < 3:
+        command += sock.recv(3 - len(command))
     return command.decode()
+
 
 def get_int(sock):
     data = bytes()
     data += sock.recv(1)
     return int.from_bytes(data, byteorder='big')
-    
+
+
 def receive_bye(sock):
     sock.close()
 
+
 def receive_end(sock):
-    #supprimertoutcequiexiste
+    # supprimertoutcequiexiste
     pass
+
 
 def send_nme(sock, name):
     message = bytes()
@@ -37,33 +42,48 @@ def send_nme(sock, name):
     message += name.encode()
     sock.send(message)
 
+
 def send_mov(sock, movements):
     message = bytes()
     message += 'MOV'.encode()
+    print(movements)
     message += bytes([len(movements)])
     for mov in movements:
-        message += mov.coord_init.encode()
-        message += mov.number_indiv.encode()
-        message += mov.coord_arriv.encode()
+        message += bytes([mov.coord_init[0]])
+        message += bytes([mov.coord_init[1]])
+        message += bytes([mov.number_indiv])
+        message += bytes([mov.coord_arriv[0]])
+        message += bytes([mov.coord_arriv[1]])
     sock.send(message)
 
 
-def recieve_set(sock):
+class mov:
+    def __init__(self, init, numb, arriv):
+        self.coord_arriv = arriv
+        self.coord_init = init
+        self.number_indiv = numb
+
+
+
+def receive_set(sock):
     n = get_int(sock)
     m = get_int(sock)
-    return n,m
+    return n, m
 
-def recieve_hum(sock):
+
+def receive_hum(sock):
     n_humans = get_int(sock)
-    humans_coords = [(get_int(sock),get_int(sock)) for i in range(n_humans)]
+    humans_coords = [(get_int(sock), get_int(sock)) for i in range(n_humans)]
     return humans_coords
 
-def recieve_hme(sock):
+
+def receive_hme(sock):
     x = get_int(sock)
     y = get_int(sock)
-    return x,y
+    return x, y
 
-def recieve_udp(sock):
+
+def receive_upd(sock):
     n_changes = get_int(sock)
     changes_coords = []
     for i in range(n_changes):
@@ -73,9 +93,9 @@ def recieve_udp(sock):
         n_humans = get_int(sock)
         n_vampires = get_int(sock)
         n_werewolf = get_int(sock)
-        dic['coords'] = (x,y)
+        dic['coords'] = (x, y)
 
-        dic['species'] = None # werewolfes humans vampires
+        dic['species'] = None  # werewolfes humans vampires
         species_alive = 0
         if n_humans:
             dic['species'] = "humans"
@@ -90,34 +110,35 @@ def recieve_udp(sock):
         if species_alive > 1:
             raise ValueError(f"Too many species: {n_humans} {n_vampires} {n_werewolf}")
 
-        
-
         dic['number'] = n_humans + n_vampires + n_werewolf
         changes_coords.append(dic)
     return changes_coords
 
-def process_command(command : str, sock):
+
+def process_command(command: str, sock):
     print(f"I recieved the command {command}")
     if command == 'SET':
-        print(recieve_set(sock))
-    elif command == 'HUM':
-        print(recieve_hum(sock))
-    elif command == "HME":
-        print(recieve_hme(sock))
-    elif command == 'MAP' or command == 'UDP':
-        print(recieve_udp(sock))
-        
-def run(name):
 
+        print(receive_set(sock))
+    elif command == 'HUM':
+        print(receive_hum(sock))
+    elif command == "HME":
+        print(receive_hme(sock))
+    elif command == 'MAP' or command == 'UPD':
+        print(receive_upd(sock))
+        print(command)
+        if command == 'UPD':
+            ask_moves()
+
+def run(name):
     config = load_config()
     IP, port = config["IP"], int(config["port"])
-
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((IP, port))
     print("Connect")
     send_nme(sock, name)
-    
+
     while True:
         command = get_command(sock)
         process_command(command, sock)
