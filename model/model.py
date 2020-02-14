ad284937e0f88250aa61d9e20fd5cfb0256a51c0
Carole El-Kurdi <carole.kurdi@gmail.com>
2020-02-14 12:33:16 +0100

    Calculate output of cell if attacked (w/ random battle)

--- model/model.py
+++ model/model.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class Board:
     """Class modelizing the board
     """
@@ -99,6 +101,71 @@ class Cell:
 
         self.creature = creature_name
         self.number = number_creatures
+    
+    def output_if_attacked(self, attacker_species, attacker_qty):
+        """Method that gives the hypothetic output state of the cell if it is under attack .
+        
+        Arguments:
+            attacker_species {string} -- name of the creature attacking the cell
+            attacker_qty {int}  -- number of attackers  
+        """  
+        if self.creature == "humans":
+            if attacker_qty >= self.number :
+                output = {"creature":attacker_species, "number": self.number + attacker_qty}
+            else:
+                output = __random_battle() 
+        elif attacker_qty >= 1.5*self.number:
+            output = {"creature":attacker_species, "number": attacker_qty}
+        else:
+            output = __random_battle()
+        return output
+    
+    def __random_battle(attacker_species, attacker_qty, defender_species, defender_qty):
+        """Private method that simulates a random battle.
+        
+        Arguments:
+            attacker_species {string} -- name of the creature attacking the cell
+            attacker_qty {int}  -- number of attackers
+            defender_species {string} -- name of the creature attacking the cell
+            defender_qty {int}  -- number of attackers 
+        """ 
+        # 1. Calculate probability p
+        if attacker_qty = defender_qty :
+            p = 0.5
+        elif attacker_qty < defender_qty:
+            p = attacker_qty / (2*defender_qty)
+        else:
+            p= -0.5 + attacker_qty/defender_qty
+        
+        # 2. Determine winner
+        winner_species = np.random.choice([attacker_species, defender_species], 1, p=[p, 1-p])[0]
+       
+        # 3. Count winner survivors
+        if winner_species==attacker_species:
+            # Attackers win
+            number_winners_survivors =  __get_total_survivors(attacker_species, p)
+            if defender_species == "humans":
+                number_humans_survivors = __get_total_survivors(defender_qty, p)
+                number_winners_survivors += number_humans_survivors
+        else:
+            # Defenders win
+            number_winners_survivors =  __get_total_survivors(defender_qty, 1-p)
+        
+        return {"creature":winner_species, "number": number_winners_survivors}
+
+    def __get_total_survivors(initial_quantity, proba_to_survive):
+        """Private method that gets the number of survivors of a given quantity of creatures according to a certain probability to survive.
+        
+        Arguments:
+            initial_quantity {int} -- initial numbers of creatures
+            proba_to_survive {float}  -- probability to survive
+        """ 
+        total_survivors = 0
+            for i in range(initial_quantity):
+                total_survivors += np.random.choice([1, 0], 1, p=[proba_to_survive, 1-proba_to_survive])[0]
+        return total_survivors
+        
+
 
 class GameInfo:
     """Class for general game information
