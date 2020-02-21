a1f80324447b508ffade149da98275ff6aace5e4
SaulLu <lucilesaul.com@gmail.com>
2020-02-21 11:29:17 +0100

    correct docstrings

--- player.py
+++ player.py
@@ -18,58 +18,12 @@ class Player():
         self.opponent_name = None
         self.first_position = None
         self.strategy = strategy
-
-    def init_board(self, max_x, max_y):
-        """Function to init the board when SET is received
-        Arguments:
-        max_x {int} -- number of columns
-        max_y {int} -- number of rows
-        """
-        self.strategy.current_board = Board(max_x, max_y)
-
-    def init_humans(self, humans_coords):
-        #useless apiori
-        pass
-
-    def init_home(self, x, y):
-        """Function to store our info when HME is received
-        
-        Arguments:
-            x {int} -- column
-            y {int} -- row
-        """
-        self.first_position = (x,y)
-
-    def init_game(self):
-        """Initialize the game with the info received by the command 
-        """
-        config_connector = load_config_connect()
-        config_player = self.__load_config()
-        cl = Client(config_player['player_name'],config_connector['IP'],int(config_connector['port']))
-        cl.start()
-        #time.sleep(1)
-
-        board_size = cl.get_board_size()
-        self.init_board(board_size)
-        print(f"I just got the board size: {board_size}")
-    
-        coords_start = cl.get_start_location()
-        self.init_home(coords_start)
-        print(f"I just got the starting location: {coords_start}")
-
-        initial_setup = cl.get_board_changes(timeout = 5*60)
-        for dico_cell in initial_setup:
-            if dico_cell['coords'] == self.first_position:
-                self.our_name = dico_cell['species']
-                assert((self.our_name in ["werewolves", "vampires"]), 'Not allowed creature name')
-                self.opponent_name = "werewolves" if self.our_name == "vampires" else "vampires"
-        
-        self.strategy.update_board(initial_setup, self.our_name)
-        print(f"I just got the initial setup : {initial_setup}")
     
     def play(self):
+        """Initializes the game and loops between our turn and ennemy's turn until the end 
+        """
 
-        self.init_game()
+        self.__init_game()
 
         while True:
             while not(cl.is_my_turn() or cl.has_game_ended()):
@@ -83,9 +37,64 @@ class Player():
                 print("game has ended")
                 break
 
-        cl.close()        
+        cl.close()
+
+    def __init_game(self):
+        """Initialize the game with the info received by the client 
+        """
+        config_connector = load_config_connect()
+        config_player = self.__load_config()
+        cl = Client(config_player['player_name'],config_connector['IP'],int(config_connector['port']))
+        cl.start()
+        #time.sleep(1)
+
+        board_size = cl.get_board_size()
+        self.__init_board(board_size)
+        print(f"I just got the board size: {board_size}")
+    
+        coords_start = cl.get_start_location()
+        self.__init_home(coords_start)
+        print(f"I just got the starting location: {coords_start}")
+
+        initial_setup = cl.get_board_changes(timeout = 5*60)
+        for dico_cell in initial_setup:
+            if dico_cell['coords'] == self.first_position:
+                self.our_name = dico_cell['species']
+                assert((self.our_name in ["werewolves", "vampires"]), 'Not allowed creature name')
+                self.opponent_name = "werewolves" if self.our_name == "vampires" else "vampires"
+        
+        self.strategy.update_board(initial_setup, self.our_name)
+        print(f"I just got the initial setup : {initial_setup}")
+    
+    def __init_board(self, max_x, max_y):
+        """Function to init the board when SET is received
+        Arguments:
+        max_x {int} -- number of columns
+        max_y {int} -- number of rows
+        """
+        self.strategy.current_board = Board(max_x, max_y)
+
+    def __init_humans(self, humans_coords):
+        #useless apiori
+        pass
+
+    def __init_home(self, x, y):
+        """Function to store our info when HME is received
+        
+        Arguments:
+            x {int} -- column
+            y {int} -- row
+        """
+        self.first_position = (x,y)
+
+
         
     def __load_config(self):
+        """Function to load the player config
+        
+        Returns:
+            Json object -- the content of the config file
+        """
         config_path = pathlib.Path(__file__).parent.parent / "config.json"
         with open(config_path) as f:
             content = json.load(f)
