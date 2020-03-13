91604a42cd3a054b38c04d4c58b29884be86634c
SaulLu <lucilesaul.com@gmail.com>
2020-03-13 19:05:55 +0100

    save move already compute

--- models/target_engine.py
+++ models/target_engine.py
@@ -64,17 +64,18 @@ def get_feasible_targets(board: Board, creature):
     min_takeover_factor = {engine.reverse_creature(creature): 1.5, 'humans': 1}
 
 def targets_to_moves(targets_scenarios_list, board:Board):
+    calculate_moves = dict()
     mov_scenarios_list = []
     for targets_scenario in targets_scenarios_list:
         mov_scenario = []
         for target_dict in targets_scenario:
-            mov_temp = target_to_move(board, **target_dict)
+            mov_temp = target_to_move(board=board, calculate_moves=calculate_moves, **target_dict)
             if mov_temp is not None:
                 mov_scenario.append(mov_temp)
         mov_scenarios_list.append(mov_scenario)
     return(mov_scenarios_list)
 
-def target_to_move(board: Board, start, target, number:int):
+def target_to_move(board: Board, calculate_moves: dict, start:(int,int), target:(int,int), number:int):
     """
     
     Arguments:
@@ -85,34 +86,42 @@ def target_to_move(board: Board, start, target, number:int):
     Return:
         Mov -- an instance of Mov object
     """
-    arriv = None
-    # if target next to me
+    key = start + target
 
-    offsets = list(product([0,1,-1],repeat = 2))
-    offsets.remove((0,0))
-    offsets = np.array(offsets)
-
-    poss_arriv = start + offsets
-
-    if target in poss_arriv and board.grid[target[0],target[1]].creature != 'us':
-        arriv = target
-        return (Mov(start, number, tuple(arriv)))
-
-    dist = get_distance_between_array_cells(poss_arriv, target)
-    scores = np.column_stack((dist, poss_arriv))
-    scores.view('i8,i8,i8').sort(order=['f0'], axis=0)
-
-    for poss_coord in scores[:,1:]:
-        if ((poss_coord[0] >= 0) and (poss_coord[0] < board.max_x) and \
-            (poss_coord[1] >= 0) and (poss_coord[1] < board.max_y) and \
-            (not board.grid[poss_coord[0],poss_coord[1]].creature)):
-            arriv = poss_coord
-            break
-
-    if arriv.all():
-        return(Mov(start, number, tuple(arriv)))
+    if key in calculate_moves:
+        return(calculate_moves[key])
+    
     else:
-        return(None)
+        arriv = None
+
+        offsets = list(product([0,1,-1],repeat = 2))
+        offsets.remove((0,0))
+        offsets = np.array(offsets)
+
+        poss_arriv = start + offsets
+
+        if target in poss_arriv and board.grid[target[0],target[1]].creature != 'us':
+            arriv = target
+            calculate_moves[key] = Mov(start, number, tuple(arriv))
+            return (calculate_moves[key])
+
+        dist = get_distance_between_array_cells(poss_arriv, target)
+        scores = np.column_stack((dist, poss_arriv))
+        scores.view('i8,i8,i8').sort(order=['f0'], axis=0)
+
+        for poss_coord in scores[:,1:]:
+            if ((poss_coord[0] >= 0) and (poss_coord[0] < board.max_x) and \
+                (poss_coord[1] >= 0) and (poss_coord[1] < board.max_y) and \
+                (not board.grid[poss_coord[0],poss_coord[1]].creature)):
+                arriv = poss_coord
+                break
+
+        if arriv.all():
+            calculate_moves[key] = Mov(start, number, tuple(arriv))
+        else:
+            calculate_moves[key] = None
+        return(calculate_moves[key])
 
 def get_distance_between_array_cells(array_pos_cell1, array_pos_cell2):
-        return np.max(np.abs(array_pos_cell1-array_pos_cell2), axis=1)
+    #asert
+    return np.max(np.abs(array_pos_cell1-array_pos_cell2), axis=1)
