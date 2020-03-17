1020cdaf160b84b55603591f24af286caccab3d1
Laura <33421605+HennequinLaura@users.noreply.github.com>
2020-03-17 16:55:13 +0100

    improve ckeck moves

--- models/check_engine.py
+++ models/check_engine.py
@@ -17,7 +17,7 @@ def check_mov_validity(board : Board, mov : Mov):
     if (mov.n_creatures < 0):
         print("Negative number of creatures to move")
         return (False)
-    if (mov.n_creatures > board.grid[mov.initial_coordinates]):
+    if (mov.n_creatures > board.grid[mov.initial_coordinates].number):
         print("Too many creatures to move")
         return (False)
     if  mov is None: 
@@ -30,10 +30,21 @@ def check_moves_validity(board : Board, moves : list):
     if moves is None:
         print("This list of moves is None")
         return False
-        
+
+    creatures_leaving = {}
     for mov in moves: 
+        if not(mov.initial_coordinates in creatures_leaving):                
+            creatures_leaving[mov.initial_coordinates] = mov.n_creatures
+        else:
+            creatures_leaving[mov.initial_coordinates] += mov.n_creatures
         if not(check_mov_validity(board, mov)):
             return False
+        
+    for coord in creatures_leaving:
+        if board[coord].number < creatures_leaving[coord]:
+            print "Trying to move too many creatures"
+            return False
+            
     return True
 
 def check_board_validity(move):
