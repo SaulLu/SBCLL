7b0df34e6717d71db9a14aa53f393c00cc546584
EtWnn <eti1ww@gmail.com>
2020-03-17 19:51:44 +0100

    fix next_best

--- strategies/heuristics.py
+++ strategies/heuristics.py
@@ -5,7 +5,8 @@ import models.engine as engine
 import models.target_engine as target_engine
 from models.board import Board
 
-def naive_heuristic(board : Board):
+
+def naive_heuristic(board: Board):
     """ A naive heuristic : calculate the difference between the number of us left and the number if them left.
     
     Arguments:
@@ -18,22 +19,25 @@ def naive_heuristic(board : Board):
 
     return number_of_us - number_of_them
 
-def distance_target_difference_heuristic(board : Board):
+
+def distance_target_difference_heuristic(board: Board):
     feasible_targets_us = target_engine.get_feasible_targets(board, "us")
     feasible_targets_them = target_engine.get_feasible_targets(board, "them")
 
-    score_us = __get_score_distance_target(board, feasible_targets_us) 
-    score_them = __get_score_distance_target(board, feasible_targets_them) 
+    score_us = __get_score_distance_target(board, feasible_targets_us)
+    score_them = __get_score_distance_target(board, feasible_targets_them)
 
     return score_us - score_them
 
-def distance_target_heuristic(board : Board):
-    feasible_targets_us = target_engine.get_feasible_targets(board, "us")
-    return __get_score_distance_target(board, feasible_targets_us) 
 
-def __get_score_distance_target(board : Board, dict_feasible_targets : dict):
+def distance_target_heuristic(board: Board):
+    feasible_targets_us = target_engine.get_feasible_targets(board, "us")
+    return __get_score_distance_target(board, feasible_targets_us)
+
+
+def __get_score_distance_target(board: Board, dict_feasible_targets: dict):
     score = 0
-    for pos,list_targets in dict_feasible_targets:
+    for pos, list_targets in dict_feasible_targets:
         for pos_target in list_targets:
-            score += board.grid[pos_target].number / (engine.get_distance_between_cells(pos, pos_target)**2) 
+            score += board.grid[pos_target].number / (engine.get_distance_between_cells(pos, pos_target) ** 2)
     return score
--- strategies/next_best_strategy.py
+++ strategies/next_best_strategy.py
@@ -5,16 +5,23 @@ from strategies.abstract_strategy import Strategy
 import models.engine as engine
 
 
+
+
 class NextBestStrategy(Strategy):
     def __init__(self, max_x, max_y, heuristic):
         super().__init__(max_x, max_y, heuristic)
+
+    def probability_score(self, probability_board):
+        board, p = probability_board
+        return p * self.heuristic(board)
 
     def next_moves(self, think_time):
         all_possible_turns = np.array(engine.get_possible_turns(self.current_board, 'us'))
         all_possible_boards = [engine.create_possible_boards_many_moves(
             self.current_board, turn, 'us') for turn in all_possible_turns]
 
-        graded_boards = [self.heuristic(b) for b in all_possible_boards]
+        graded_boards = [np.sum([p*self.heuristic(board) for board,p in probability_boards]) for probability_boards
+                         in all_possible_boards]
         best_board_index = np.argmax(graded_boards)
         time.sleep((think_time / 1000)/10)
         return all_possible_turns[best_board_index]
