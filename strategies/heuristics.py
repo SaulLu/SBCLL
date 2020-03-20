6c002d0ed9ebeb30436b2daa6341dc5b7b330190
EtWnn <eti1ww@gmail.com>
2020-03-20 09:05:13 +0100

    update player and correct heuristics

--- strategies/heuristics.py
+++ strategies/heuristics.py
@@ -21,23 +21,21 @@ def naive_heuristic(board: Board):
 
 
 def distance_target_difference_heuristic(board: Board):
-    feasible_targets_us = target_engine.get_feasible_targets(board, "us")
-    feasible_targets_them = target_engine.get_feasible_targets(board, "them")
-
-    score_us = __get_score_distance_target(board, feasible_targets_us)
-    score_them = __get_score_distance_target(board, feasible_targets_them)
-
-    return score_us - score_them
+    return naive_heuristic(board) + distance_target_heuristic(board, 'us') - distance_target_heuristic(board, 'them')
 
 
-def distance_target_heuristic(board: Board):
-    feasible_targets_us = target_engine.get_feasible_targets(board, "us")
-    return __get_score_distance_target(board, feasible_targets_us)
+def distance_target_heuristic(board: Board, creature='us'):
+    targets_coordinates = target_engine.get_available_targets(creature, board)
+    takeover_targets = {coordinate: target_engine.get_min_takeover(board.get_cell(coordinate))
+                        for coordinate in targets_coordinates}
+    attackers = {coordinate: board.get_cell(coordinate).number for coordinate in board.creatures_list[creature]}
+    targets_per_attackers = target_engine.get_targets_per_attackers(takeover_targets, attackers)
+    return naive_heuristic(board) + __get_score_distance_target(board, targets_per_attackers)
 
 
-def __get_score_distance_target(board: Board, dict_feasible_targets: dict):
+def __get_score_distance_target(board: Board, targets_per_attackers: dict):
     score = 0
-    for pos, list_targets in dict_feasible_targets:
+    for pos, list_targets in targets_per_attackers.items():
         for pos_target in list_targets:
             score += board.grid[pos_target].number / (engine.get_distance_between_cells(pos, pos_target) ** 2)
     return score
