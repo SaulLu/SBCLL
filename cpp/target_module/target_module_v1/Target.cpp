77ad7bcd82113246ee87cb140d6b02bb0d8c2e19
EtWnn <eti1ww@gmail.com>
2020-03-26 13:49:33 +0100

    Target class

--- cpp/target_module/target_module_v1/Target.cpp
+++ cpp/target_module/target_module_v1/Target.cpp
@@ -3,15 +3,23 @@
 
 using namespace std;
 
-Target::Target(const int x, const int y, const Creature creature, const int number)
-		: m_x(x), m_y(y), m_creature(creature), m_number(number), m_min_takeover(MinTakeOver(creature, number)), m_id(getNextId())
+int Target::s_next_id = 0;
+
+Target::Target() 
+	: m_location({ NULL, NULL }), m_creature(Creature::Empty), m_number(NULL), m_takeover(NULL), m_id(NULL)
 {
 
 }
 
-int Target::getMinTakeOver()
+Target::Target(const std::array<int, 2> location, const Creature creature, const int number, const int id)
+		: m_location(location), m_creature(creature), m_number(number), m_takeover(computeTakeOver(creature, number)), m_id(id)
 {
-	return m_min_takeover;
+
+}
+
+int Target::getTakeOver()
+{
+	return m_takeover;
 }
 
 void Target::addAttacker(const int attacker_id)
@@ -19,11 +27,44 @@ void Target::addAttacker(const int attacker_id)
 	m_attackers_ids.push_back(attacker_id);
 }
 
-void Target::removeAttacker(const int attacker_id)
+bool Target::removeAttacker(const int attacker_id)
 {
 	vector<int>::iterator position = find(m_attackers_ids.begin(), m_attackers_ids.end(), attacker_id);
 	if (position != m_attackers_ids.end())
+	{
 		m_attackers_ids.erase(position);
+		return m_attackers_ids.size() == 0;
+	}
+		
+	return false;
+}
+
+void Target::purgeAttackers(std::map<int, Target>& targets, std::map<int, Attacker>& attackers)
+{
+	int k = 0;
+	while (k < m_attackers_ids.size())
+	{
+		int attacker_id = m_attackers_ids[k];
+		if (attackers[attacker_id].getNTargets() > 1)
+		{
+			m_attackers_ids.erase(m_attackers_ids.begin() + k);
+			attackers[attacker_id].removeTarget(m_id, targets, attackers);
+		}
+		else 
+		{
+			k++;
+		}
+	}
+}
+
+void Target::clearAttackers()
+{
+	m_attackers_ids.clear();
+}
+
+const std::array<int, 2> Target::getLocation()
+{
+	return m_location;
 }
 
 int Target::getNextId()
@@ -32,10 +73,20 @@ int Target::getNextId()
 	return s_next_id;
 }
 
-int Target::MinTakeOver(const Creature creature, const int number)
+const std::vector<int>* Target::getAttackersIdsPointer()
 {
-	if (creature == Humans) {
+	return &m_attackers_ids;
+}
+
+Target Target::operator=(const Target& t)
+{
+	return Target(t.m_location, t.m_creature, t.m_number, t.m_id);
+}
+
+int Target::computeTakeOver(const Creature creature, const int number)
+{
+	if (creature == Creature::Humans) {
 		return number;
 	}
-	return ceil(1.5 * number);
+	return (int)ceil(1.5 * number);
 }
--- cpp/target_module/target_module_v1/Target.h
+++ cpp/target_module/target_module_v1/Target.h
@@ -1,6 +1,7 @@
 #ifndef TARGET_H_INCLUDED 
 #define TARGET_H_INCLUDED
 
+#include <array>
 #include <vector>
 #include <map>
 
@@ -11,22 +12,31 @@ class Attacker;
 class Target
 {
 public:
-	Target(const int x, const int y, const Creature creature, const int number);
-	int getMinTakeOver();
+	Target();
+	Target(const std::array<int, 2> location, const Creature creature, const int number, const int id);
+	int getTakeOver();
 	void addAttacker(const int attacker_id);
-	void removeAttacker(const int attacker_id);
+	bool removeAttacker(const int attacker_id);
+	void purgeAttackers(std::map<int, Target>& targets, std::map<int, Attacker>& attackers);
+	void clearAttackers();
+	const std::array<int, 2> getLocation();
+
+	const std::vector<int>* getAttackersIdsPointer();
+
+	Target operator=(const Target& t);
+
 	static int getNextId();
+
 private:
 	static int s_next_id;
 	const int m_id;
-	const int m_x;
-	const int m_y;
+	const std::array<int, 2> m_location;
 	const Creature m_creature;
 	const int m_number;
-	const int m_min_takeover;
+	const int m_takeover;
 	std::vector<int> m_attackers_ids;
 
-	static int MinTakeOver(const Creature creature, const int number);
+	static int computeTakeOver(const Creature creature, const int number);
 };
 
 #endif
