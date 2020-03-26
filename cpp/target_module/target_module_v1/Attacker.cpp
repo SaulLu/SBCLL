155fb7bc23955c86882429e29c76c125a253a702
EtWnn <eti1ww@gmail.com>
2020-03-26 13:49:13 +0100

    Attacker class

--- cpp/target_module/target_module_v1/Attacker.cpp
+++ cpp/target_module/target_module_v1/Attacker.cpp
@@ -3,34 +3,46 @@
 
 using namespace std;
 
-int Attacker::s_next_id = -1;
+int Attacker::s_next_id = 0;
 
-Attacker::Attacker(const int x, const int y, const int number) : m_x(x), m_y(y), m_number(number), m_id(getNextId())
+Attacker::Attacker() : m_location({ NULL, NULL }), m_number(NULL), m_id(NULL)
+{
+
+}
+
+Attacker::Attacker(const std::array<int, 2> location, const int number, const int id) : m_location(location), m_number(number), m_id(id)
 {
 }
 
-void Attacker::udpate(int number)
+void Attacker::lowerNumber(int number_sent)
 {
-	m_number = number;
+	m_number -= number_sent;
 }
 
-void Attacker::updateTargets(std::map<int, Target>& targets_map)
+bool Attacker::updateTargets(std::map<int, Target>& targets, std::map<int, Attacker>& attackers)
 {
-	auto vector_begin = m_targets_ids.begin();
+	bool infer = false;
 	int n_targets = m_targets_ids.size();
 	int k = 0;
 	while (k < n_targets) {
 		int target_id = m_targets_ids[k];
-		if (targets_map[target_id].getMinTakeOver() > m_number) {
-			m_targets_ids.erase(vector_begin + k);
+		if (targets[target_id].getTakeOver() > m_number) {
+			m_targets_ids.erase(m_targets_ids.begin() + k);
 			n_targets--;
-			targets_map[target_id].removeAttacker(m_id);
+			infer = infer || targets[target_id].removeAttacker(m_id);
 		}
 		else
 		{
 			k++;
 		}
 	}
+
+	if (m_targets_ids.size() == 1)
+	{
+		targets[m_targets_ids[0]].purgeAttackers(targets, attackers);
+	}
+
+	return infer;
 }
 
 void Attacker::addTarget(const int target_id)
@@ -38,11 +50,59 @@ void Attacker::addTarget(const int target_id)
 	m_targets_ids.push_back(target_id);
 }
 
-void Attacker::removeTarget(const int target_id)
+void Attacker::removeTarget(const int target_id, std::map<int, Target>& targets, std::map<int, Attacker>& attackers)
 {
 	vector<int>::iterator position = find(m_targets_ids.begin(), m_targets_ids.end(), target_id);
 	if (position != m_targets_ids.end())
+	{
 		m_targets_ids.erase(position);
+
+		if (m_targets_ids.size() == 1)
+		{
+			targets[target_id].purgeAttackers(targets, attackers);
+		}
+	}
+		
+}
+
+const int Attacker::getId()
+{
+	return m_id;
+}
+
+const int Attacker::getNumber()
+{
+	return m_number;
+}
+
+const int Attacker::getNTargets()
+{
+	return m_targets_ids.size();
+}
+
+const std::array<int, 2> Attacker::getLocation()
+{
+	return m_location;
+}
+
+const int Attacker::getMinTakeOvers(std::map<int, Target>& targets, int id_to_exclude)
+{
+	int min_takerovers = INT_MAX;
+	for (auto& target_id : m_targets_ids)
+	{
+		if (target_id != id_to_exclude)
+		{
+			int temp_takeover = targets[target_id].getTakeOver();
+			if (temp_takeover < min_takerovers)
+			{
+				min_takerovers = temp_takeover;
+			}
+		}
+	}
+
+	if (min_takerovers == INT_MAX) return 0;
+
+	return min_takerovers;
 }
 
 int Attacker::getNextId()
@@ -51,3 +111,7 @@ int Attacker::getNextId()
 	return s_next_id;
 }
 
+Attacker Attacker::operator=(const Attacker& a)
+{
+	return Attacker(a.m_location, a.m_number, a.m_id);
+}
--- cpp/target_module/target_module_v1/Attacker.h
+++ cpp/target_module/target_module_v1/Attacker.h
@@ -1,27 +1,36 @@
 #ifndef ATTACKER_H_INCLUDED 
 #define ATTACKER_H_INCLUDED 
 
+#include <array>
 #include <vector>
 #include <map>
 #include <algorithm>
+#include <limits.h>
 
 class Target;
 class Attacker
 {
 public:	
-	Attacker(const int x, const int y, const int number);
-	void udpate(int number);
-	void updateTargets(std::map<int, Target>& targets_map);
+	Attacker();
+	Attacker(const std::array<int,2> location, const int number, const int id);
+	void lowerNumber(int number_sent);
+	bool updateTargets(std::map<int, Target>& targets, std::map<int, Attacker>& attackers);
 	void addTarget(const int target_id);
-	void removeTarget(const int target_id);
+	void removeTarget(const int target_id, std::map<int, Target>& targets, std::map<int, Attacker>& attackers);
+	const int getId();
+	const int getNumber();
+	const int getNTargets();
+	const std::array<int, 2> getLocation();
+	const int getMinTakeOvers(std::map<int, Target>& targets, int id_to_exlud = -1);
 
 	static int getNextId();
+
+	Attacker operator=(const Attacker& a);
 
 private:
 	static int s_next_id;
 	const int m_id;
-	const int m_x;
-	const int m_y;
+	const std::array<int, 2> m_location;
 	int m_number;
 	std::vector<int> m_targets_ids;
 };
