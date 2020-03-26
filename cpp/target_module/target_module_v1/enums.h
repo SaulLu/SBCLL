76b3a7d04f93149b39e8bb0fa0673e3c148d391f
EtWnn <eti1ww@gmail.com>
2020-03-26 13:49:48 +0100

    Error Target enum

--- cpp/target_module/target_module_v1/enums.h
+++ cpp/target_module/target_module_v1/enums.h
@@ -1,15 +1,23 @@
 #ifndef ENUMS_H_INCLUDED 
 #define ENUMS_H_INCLUDED 
 
-enum Creature : int
+enum class Creature : int
 {
-	None = 0,
+	Empty = 0,
 	Humans = 1,
 	Us = 2,
 	Them = 3
 };
 
-
+enum class TargetError : int
+{
+	noError = 0,
+	LeftOver = 1,
+	OverSend = 2,
+	MissedOpportunity = 3,
+	OverTargeted = 4,
+	UnderTargeted = 5
+};
 
 
 #endif
